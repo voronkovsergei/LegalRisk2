0f9a2312fd0463f52c14d6f43d2eddd50f892985
rm <rmermin@yahoo.com>
2025-04-29 12:20:53 +0300

    Write constructor

--- src/main/java/com/example/legalrisk/pipelines/LegalRiskAnalysisPipeline.java
+++ src/main/java/com/example/legalrisk/pipelines/LegalRiskAnalysisPipeline.java
@@ -13,4 +13,8 @@ public class LegalRiskAnalysisPipeline implements AnalysisPipeline{
 
     List<Analyze> tasks;
 
+    LegalRiskAnalysisPipeline(){
+        System.out.println("initialize");
+    }
+
 }
